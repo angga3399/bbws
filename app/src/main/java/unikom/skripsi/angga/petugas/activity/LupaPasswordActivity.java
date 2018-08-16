fa56404334e2f1a57a9f64b9792765d1a3929992
angga <angga3399@gmail.com>
2018-08-16 15:32:58 +0700

    "update UI - angga"

--- app/src/main/java/unikom/skripsi/angga/petugas/activity/LupaPasswordActivity.java
+++ app/src/main/java/unikom/skripsi/angga/petugas/activity/LupaPasswordActivity.java
@@ -1,10 +1,13 @@
 package unikom.skripsi.angga.petugas.activity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import retrofit2.Call;
@@ -18,6 +21,9 @@ import unikom.skripsi.angga.petugas.rest.ApiInterface;
 public class LupaPasswordActivity extends AppCompatActivity {
     Button btn_kirim;
     EditText txt_username;
+    TextView txt_login;
+    ProgressDialog pDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +31,7 @@ public class LupaPasswordActivity extends AppCompatActivity {
 
         btn_kirim = findViewById(R.id.btn_kirim);
         txt_username = findViewById(R.id.txt_username);
+        txt_login = findViewById(R.id.txt_login);
 
         btn_kirim.setOnClickListener(new View.OnClickListener() {
 
@@ -46,19 +53,42 @@ public class LupaPasswordActivity extends AppCompatActivity {
     }
 
     private void checkLupaPassword(String username) {
+
+        pDialog = new ProgressDialog(this);
+        pDialog.setCancelable(false);
+        pDialog.setMessage("Mengirim ke Email Anda");
+        showDialog();
         Call<MessageModel> api = ApiClient.getClient().create(ApiInterface.class).postLupa(username);
         api.enqueue(new Callback<MessageModel>() {
             @Override
             public void onResponse(Call<MessageModel> call, Response<MessageModel> response){
                 if (response.isSuccessful()){
+                    hideDialog();
                     Toast.makeText(LupaPasswordActivity.this, response.body().getMessage(), Toast.LENGTH_SHORT).show();
                 }
             }
 
             @Override
             public void onFailure(Call<MessageModel> call, Throwable t) {
+                hideDialog();
                 Toast.makeText(LupaPasswordActivity.this, t.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    public void login(View view) {
+        Intent intent = new Intent(LupaPasswordActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private void showDialog() {
+        if (!pDialog.isShowing())
+            pDialog.show();
+    }
+
+    private void hideDialog() {
+        if (pDialog.isShowing())
+            pDialog.dismiss();
     }
 }
